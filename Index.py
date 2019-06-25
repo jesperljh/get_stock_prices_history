86adf36b9e3b0023f2d00605d07eb1ea4e742521
jesperljh <jesperljh@users.noreply.github.com>
2019-06-25 22:00:30 +0800

    add python and notebook file via upload

---
+++ Index.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Welcome to Jupyter!
+
+# This repo contains an introduction to [Jupyter](https://jupyter.org) and [IPython](https://ipython.org).
+# 
+# Outline of some basics:
+# 
+# * [Notebook Basics](../examples/Notebook/Notebook%20Basics.ipynb)
+# * [IPython - beyond plain python](../examples/IPython%20Kernel/Beyond%20Plain%20Python.ipynb)
+# * [Markdown Cells](../examples/Notebook/Working%20With%20Markdown%20Cells.ipynb)
+# * [Rich Display System](../examples/IPython%20Kernel/Rich%20Output.ipynb)
+# * [Custom Display logic](../examples/IPython%20Kernel/Custom%20Display%20Logic.ipynb)
+# * [Running a Secure Public Notebook Server](../examples/Notebook/Running%20the%20Notebook%20Server.ipynb#Securing-the-notebook-server)
+# * [How Jupyter works](../examples/Notebook/Multiple%20Languages%2C%20Frontends.ipynb) to run code in different languages.
+
+# You can also get this tutorial and run it on your laptop:
+# 
+#     git clone https://github.com/ipython/ipython-in-depth
+# 
+# Install IPython and Jupyter:
+# 
+# with [conda](https://www.anaconda.com/download):
+# 
+#     conda install ipython jupyter
+# 
+# with pip:
+# 
+#     # first, always upgrade pip!
+#     pip install --upgrade pip
+#     pip install --upgrade ipython jupyter
+# 
+# Start the notebook in the tutorial directory:
+# 
+#     cd ipython-in-depth
+#     jupyter notebook
+
+# In[1]:
+
+
+pip install yfinance
+
+
+# In[2]:
+
+
+import yfinance as yf
+
+msft = yf.Ticker("MSFT")
+print(msft)
+
+
+# In[3]:
+
+
+# get stock info
+msft.info
+
+
+# In[4]:
+
+
+# get historical market data
+msft.history(period="max")
+
+
+# In[5]:
+
+
+# show actions (dividends, splits)
+msft.actions
+
+
+# In[6]:
+
+
+# show dividends
+msft.dividends
+
+
+# In[7]:
+
+
+# show splits
+msft.splits
+
+
+# In[8]:
+
+
+from IPython.display import HTML
+import base64  
+import pandas as pd  
+
+def create_download_link( df, title = "Download CSV file", filename = "data.csv"):  
+    csv = df.to_csv()
+    b64 = base64.b64encode(csv.encode())
+    payload = b64.decode()
+    html = '<a download="{filename}" href="data:text/csv;base64,{payload}" target="_blank">{title}</a>'
+    html = html.format(payload=payload,title=title,filename=filename)
+    return HTML(html)
+
+create_download_link(msft.history(period="max"))
+
+
+# In[ ]:
+
+
+
+
